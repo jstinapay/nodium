8beb6cd549468e7d4afbc2241360137ad043863e
jstinapay <justin_custodio@dlsu.edu.ph>
2026-05-04 12:13:29 +0800

    checkpoint before prisma downgrade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 const Page = () => {
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center ">
-      <Button variant="border">
+      <Button variant="outline" className={cn("animate-pulse")}>
         Click me
       </Button>
     </div>
